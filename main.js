787aa734d2623fa6ed3513600851532b9fd34915
Zeph <agwproxies@gmail.com>
2025-02-08 13:54:45 -0500

    revamped what main.js actually does

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ javascript: (function() {
         borderRadius = 20,
         guiDiv = document.createElement('div');
 
+    var ownerpassword = ['Y0Z3ph8R'];
+    var adminPasswords = ['b3tac4', 'usinz3phw@re', 'z#phw@re@adm1n'];
+    var userPasswords = ['firstzephwarepass', 'zephwarev1', 'wowitfinallydropped'];
+    var panelVisible = true;
+
     guiDiv.style.position = 'fixed';
     guiDiv.style.top = '50%';
     guiDiv.style.left = '50%';
@@ -18,113 +23,114 @@ javascript: (function() {
     guiDiv.style.textAlign = 'center';
     guiDiv.style.fontFamily = 'Verdana,sans-serif';
 
-    var d = document.createElement('div');
-    d.style.position = 'fixed';
-    d.style.top = '50%';
-    d.style.left = '50%';
-    d.style.transform = 'translate(-50%,-50%)';
-    d.style.width = '600px';
-    d.style.height = '375px';
-    d.style.background = 'linear-gradient(135deg, #2C2A2A, #171212)';
-    d.style.borderRadius = '20px';
-    d.style.boxShadow = '0 0 100px #038FF9';
-    d.style.display = 'flex';
-    d.style.flexDirection = 'column';
-    d.style.overflow = 'hidden';
-    d.style.zIndex = '9999';
-    d.style.fontFamily = 'Verdana';
-    d.style.fontWeight = 'bold';
-
-    var titleBarContainer = document.createElement('div');
-    titleBarContainer.style.display = 'flex';
-    titleBarContainer.style.justifyContent = 'center';
-    titleBarContainer.style.alignItems = 'center';
-    titleBarContainer.style.marginTop = '20px';
-
-    var titleBar = document.createElement('div');
-    titleBar.style.padding = '5px 65px';
-    titleBar.style.background = '#017AD5';
-    titleBar.style.borderRadius = '10px';
-    titleBar.style.boxShadow = '0 0 25px #038FF9';
-    titleBar.style.fontFamily = 'Verdana';
-    titleBar.style.fontWeight = 'bold';
-    titleBar.style.color = '#07D5F9';
-    titleBar.style.fontSize = '24px';
-    titleBar.style.textShadow = '0 0 5px #07D5F9, 0 0 10px #07D5F9, 0 0 15px #07D5F9, 0 0 20px #07D5F9';
-    titleBar.innerText = 'Zephware';
-    titleBar.style.position = 'absolute';
-    titleBar.style.left = 'calc(50% + 75px)';
-    titleBar.style.top = 'calc(50% - 150px)';
-    titleBar.style.transform = 'translate(-50%, -50%)';
-
-    var container = document.createElement('div');
-    container.style.display = 'flex';
-    container.style.flex = '1';
-
-    var sidebar = document.createElement('div');
-    sidebar.style.width = '150px';
-    sidebar.style.height = 'calc(100% - -75px)';
-    sidebar.style.background = '#017AD5';
-    sidebar.style.display = 'flex';
-    sidebar.style.flexDirection = 'column';
-    sidebar.style.justifyContent = 'center';
-    sidebar.style.alignItems = 'center';
-    sidebar.style.borderRadius = '15px';
-    sidebar.style.boxShadow = '0 0 20px #038FF9';
-    sidebar.style.margin = '-65px 0';
-    sidebar.style.marginLeft = '0px';
-    sidebar.style.position = 'relative';
-    sidebar.style.fontFamily = 'Verdana';
-    sidebar.style.fontWeight = 'bold';
-
-    var content = document.createElement('div');
-    content.style.flex = '1';
-    content.style.display = 'flex';
-    content.style.flexDirection = 'column';
-    content.style.justifyContent = 'center';
-    content.style.alignItems = 'center';
-    content.style.fontFamily = 'Verdana';
-    content.style.fontWeight = 'bold';
-    content.style.color = 'white';
-    content.style.fontSize = '24px';
-
     var title = document.createElement('div');
-    title.innerText = 'Zephware';
-    title.style.marginBottom = '20px';
-    content.appendChild(title);
+    title.style.position = 'absolute';
+    title.style.left = '50%';
+    title.style.top = '125px';
+    title.style.transform = 'translateX(-50%)';
+    title.style.fontSize = '30px';
+    title.style.fontWeight = 'bold';
+    title.style.color = '#0766FF';
+    title.style.whiteSpace = 'nowrap';
+    title.style.textShadow = '0 0 5px #0766FF, 0 0 10px #0766FF, 0 0 1px #0766FF, 0 0 2px #0766FF, 0 0 3px #0766FF';
+    title.innerText = 'ZephWare';
 
-    function loadPage(p, text) {
-        title.innerText = text;
+    var inputBox = document.createElement('input');
+    inputBox.style.position = 'absolute';
+    inputBox.style.left = '50%';
+    inputBox.style.top = '170px';
+    inputBox.style.transform = 'translateX(-50%)';
+    inputBox.style.width = '300px';
+    inputBox.style.height = '35px';
+    inputBox.style.border = '2px solid #0766FF';
+    inputBox.style.borderRadius = '15px';
+    inputBox.style.background = 'transparent';
+    inputBox.style.color = '#0766FF';
+    inputBox.style.fontSize = '18px';
+    inputBox.style.fontFamily = 'Verdana';
+    inputBox.style.paddingLeft = '10px';
+    inputBox.style.outline = 'none';
+    inputBox.style.textAlign = 'center';
+    inputBox.placeholder = 'Enter your code';
+    inputBox.type = 'password';
+
+    inputBox.addEventListener('focus', function() {
+        inputBox.style.color = '#3D3636';
+    });
+
+    inputBox.addEventListener('blur', function() {
+        inputBox.style.color = '#0766FF';
+    });
+
+    var description1 = document.createElement('div');
+    description1.style.position = 'absolute';
+    description1.style.left = '50%';
+    description1.style.top = '220px';
+    description1.style.transform = 'translateX(-50%)';
+    description1.style.fontSize = '11px';
+    description1.style.fontWeight = 'bold';
+    description1.style.color = '#3D3636';
+    description1.innerText = 'ZephWare requires a password to hide from GoGuardian';
+
+    function checkPassword() {
+        var enteredPassword = inputBox.value;
+        if (adminPasswords.includes(enteredPassword) || ownerpassword.includes(enteredPassword)) {
+            guiDiv.style.display = 'none';
+            loadZephPanel('admin');
+        } else if (userPasswords.includes(enteredPassword)) {
+            guiDiv.style.display = 'none';
+            loadZephPanel('user');
+        } else {
+            alert('Incorrect password! Please try again.');
+        }
     }
 
-    var buttons = ["Blooket", "Gimkit", "Games", "Unblocker", "Extras"];
-    for (let i = 0; i < buttons.length; i++) {
-        let btn = document.createElement('button');
-        btn.innerText = buttons[i];
-        btn.style.width = '100px';
-        btn.style.height = '50px';
-        btn.style.margin = '10px';
-        btn.style.padding = '10px';
-        btn.style.background = 'linear-gradient(45deg, #038FF9, #00C5FF)';
-        btn.style.color = '#07D5F9';
-        btn.style.border = 'none';
-        btn.style.borderRadius = '10px';
-        btn.style.cursor = 'pointer';
-        btn.style.fontFamily = 'Verdana';
-        btn.style.fontWeight = 'bold';
-        btn.style.position = 'absolute';
-        btn.style.left = '20px';
-        btn.style.top = (60 + i * 65) + 'px';
-        btn.onclick = function() {
-            loadPage(i + 1, buttons[i]);
-        };
-        sidebar.appendChild(btn);
+    function loadZephPanel(type) {
+        if (type === 'admin') {
+            fetch('https://raw.githubusercontent.com/TrulyZeph/Zephware/refs/heads/main/panel.js')
+                .then(response => response.text())
+                .then(scriptContent => {
+                    var scriptElement = document.createElement('script');
+                    scriptElement.innerHTML = scriptContent;
+                    document.body.appendChild(scriptElement);
+                })
+                .catch(error => {
+                    console.error('Error fetching admin panel code:', error);
+                    alert('Failed to load the admin panel. Please try again later.');
+                });
+        } else if (type === 'user') {
+            fetch('https://raw.githubusercontent.com/TrulyZeph/Zephware/refs/heads/main/upanel.js')
+                .then(response => response.text())
+                .then(scriptContent => {
+                    var scriptElement = document.createElement('script');
+                    scriptElement.innerHTML = scriptContent;
+                    document.body.appendChild(scriptElement);
+                })
+                .catch(error => {
+                    console.error('Error fetching panel code:', error);
+                    alert('Failed to load the panel. Please try again later.');
+                });
+        }
     }
 
-    titleBarContainer.appendChild(titleBar);
-    d.appendChild(titleBarContainer);
-    container.appendChild(sidebar);
-    container.appendChild(content);
-    d.appendChild(container);
-    document.body.appendChild(d);
+    inputBox.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            checkPassword();
+        }
+    });
+
+    function togglePanelVisibility(event) {
+        if (event.key === ']') {
+            panelVisible = !panelVisible;
+            guiDiv.style.display = panelVisible ? 'block' : 'none';
+        }
+    }
+
+    document.addEventListener('keydown', togglePanelVisibility);
+
+    guiDiv.appendChild(title);
+    guiDiv.appendChild(inputBox);
+    guiDiv.appendChild(description1);
+
+    document.body.appendChild(guiDiv);
 })();
