53cd8e3d38fc7e7cb6ebb73a1d1596275a4730fd
Zeph <agwproxies@gmail.com>
2026-03-01 21:12:39 -0500

    4th real legit fr link

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-javascript:window.location.href='https://storage.googleapis.com/zephware/index.html';
+javascript:window.location.href='https://pub-e28d5c5d2c53442b8f081a814488fd1e.r2.dev/index.html';
